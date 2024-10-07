ff2efb22c4421a5c693974dbaa2bad5e2197ee29
Ashkan <ashkantavangar7@gmail.com>
2024-10-08 00:05:59 +0330

    update task list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,14 @@
 
 from django.shortcuts import render,HttpResponse
 from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task
 # Create your views here.
-class TaskList(TemplateView):
+class TaskList(LoginRequiredMixin,TemplateView):
     template_name="tasks/task_list.html"
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks"] = Task.objects.all()
+        return context
+       

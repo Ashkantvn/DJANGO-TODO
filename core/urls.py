9bae243cd03347ee21226bef3a4b9db7c50208d4
Ashkan <ashkantavangar7@gmail.com>
2024-10-11 23:27:26 +0330

    Add documentation to project

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include,reverse_lazy
 from django.views.generic.base import RedirectView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TODO API",
+        default_version='v1',
+        description="API documentation for my project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("", RedirectView.as_view(url=reverse_lazy('tasks:home')), name=""),
     path('admin/', admin.site.urls),
     path("accounts/", include('accounts.urls')),
-    path("tasks/", include('tasks.urls'))
+    path("tasks/", include('tasks.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

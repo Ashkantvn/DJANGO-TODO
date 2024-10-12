4212b728c8e3230857caf357ab412f1735153d90
Ashkan <ashkantavangar7@gmail.com>
2024-10-13 00:49:46 +0330

    Create profile api for account

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TestAPIView
+from .views import ProfileAPIView
 
+
+app_name = "api-v1"
 
 urlpatterns = [
-    path("test/", TestAPIView.as_view(), name="test")
+    path("profile/", ProfileAPIView.as_view(), name="test")
 ]
--- accounts/api/v1/views.py
+++ accounts/api/v1/views.py
@@ -1,11 +1,25 @@
-from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from .serializer import UserSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 
-class TestAPIView(GenericAPIView):
+class ProfileAPIView(GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
 
-    def get(self,request, format=None):
-        return Response('OK')
+
+    def get(self,request,format=None):
+        user = self.request.user
+        data = get_object_or_404(User,id=user.id)
+        serializer = self.get_serializer(data)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+
+    
+    
 
     

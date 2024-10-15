ab3634806680fd8d81a94f96f09d58b105eb53bf
Ashkan <ashkantavangar7@gmail.com>
2024-10-16 00:44:07 +0330

    Create test for details api

--- tasks/tests/test_tasks_api.py
+++ tasks/tests/test_tasks_api.py
@@ -12,15 +12,26 @@ def authenticated_client():
     client.force_login(user=user)
     return client
 
+@pytest.fixture
+def fake_task():
+    user = User.objects.get(username = 'test')
+    fakeTask = Task.objects.create(
+        title="test title",
+        description="test content",
+        creator = user,
+    )
+    return fakeTask
+
+
 @pytest.mark.django_db
 class TestTaskAPI:
     
-    def test_get_task_list_api_response_200(self,authenticated_client):
+    def test_GET_task_list_api_response_200(self,authenticated_client):
         url = reverse("tasks:api-v1:tasks-list")
         response = authenticated_client.get(url)
         assert response.status_code == 200
 
-    def test_post_task_list_api_response_201(self,authenticated_client):
+    def test_POST_task_list_api_response_201(self,authenticated_client):
         url = reverse("tasks:api-v1:tasks-list")
         data = {
             "title": "string",
@@ -31,7 +42,9 @@ class TestTaskAPI:
         assert response.status_code == 201
 
 
-    def test_post_task_list_api_invalid_response_400(self,authenticated_client):
+    # detail api tests
+    # detail api test --- valid responses
+    def test_POST_task_list_api_invalid_response_400(self,authenticated_client):
         url = reverse("tasks:api-v1:tasks-list")
         data = {
             "title": "string",
@@ -40,12 +53,60 @@ class TestTaskAPI:
         assert response.status_code == 400
 
 
+    def test_GET_task_detail_api_valid_response_200(self,authenticated_client,fake_task):
+        url = reverse('tasks:api-v1:tasks-detail',kwargs={'pk':fake_task.pk})
+        response = authenticated_client.get(url)
+        assert response.status_code== 200
 
-    def test_get_task_detail_api_invalid_response_404(self,authenticated_client):
+
+    def test_PUT_task_detail_valid_response_200(self,authenticated_client,fake_task):
+        url = reverse('tasks:api-v1:tasks-detail',kwargs={'pk':fake_task.pk})
+        response = authenticated_client.put(url,data={
+            'title':'edited',
+            'description':'edited',
+            'status':True
+        })
+        assert response.status_code == 200
+
+    def test_PATCH_task_detail_valid_response_200(self,authenticated_client,fake_task):
+        url = reverse('tasks:api-v1:tasks-detail',kwargs={'pk':fake_task.pk})
+        response = authenticated_client.patch(url,data={
+            'status':True
+        })
+        assert response.status_code == 200
+
+
+    def test_DELETE_task_detail_valid_response_204(self,authenticated_client,fake_task):
+        url = reverse('tasks:api-v1:tasks-detail',kwargs={'pk':fake_task.pk})
+        response = authenticated_client.delete(url)
+        assert response.status_code == 204
+
+
+    # detail api test --- invalid responses
+    def test_GET_task_detail_api_invalid_response_404(self,authenticated_client):
         url = reverse("tasks:api-v1:tasks-detail",kwargs={"pk":1})
         response = authenticated_client.get(url)
         assert response.status_code == 404
 
+    def test_PUT_task_detail_invalid_response_400(self,authenticated_client,fake_task):
+        url = reverse('tasks:api-v1:tasks-detail',kwargs={'pk':fake_task.pk})
+        response = authenticated_client.put(url,data={
+            'title':'edited',
+            'status':True
+        })
+        assert response.status_code == 400
 
+    
+    def test_PATCH_task_detail_invalid_response_400(self,authenticated_client,fake_task):
+        url = reverse('tasks:api-v1:tasks-detail',kwargs={'pk':fake_task.pk})
+        response = authenticated_client.patch(url,data={
+            "title":"a"*51
+        })
+        assert response.status_code == 400
+
+    def test_DELETE_task_detail_invalid_response_204(self,authenticated_client):
+        url = reverse('tasks:api-v1:tasks-detail',kwargs={'pk':1})
+        response = authenticated_client.delete(url)
+        assert response.status_code == 404
 
     

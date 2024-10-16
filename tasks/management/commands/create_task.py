612a97e1bcd6ccc3825d1b58ae31c4a3e1c041e0
Ashkan <ashkantavangar7@gmail.com>
2024-10-16 11:36:02 +0330

    Reformat code

--- tasks/management/commands/create_task.py
+++ tasks/management/commands/create_task.py
@@ -4,20 +4,20 @@ from django.contrib.auth.models import User
 from tasks.models import Task
 import random
 
+
 class Command(BaseCommand):
     help = "Create random task"
 
-    def __init__(self, *args,**kwargs):
-        super(Command,self).__init__(*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
         self.root_user = User.objects.get(username="root")
         self.fake = Faker()
 
-
     def handle(self, *args, **options):
-        for _ in range(0,10):
+        for _ in range(0, 10):
             Task.objects.create(
-                title = self.fake.word(),
-                description = self.fake.paragraph(nb_sentences=1),
-                done = random.choice([True,False]),
-                creator = self.root_user
+                title=self.fake.word(),
+                description=self.fake.paragraph(nb_sentences=1),
+                done=random.choice([True, False]),
+                creator=self.root_user,
             )
